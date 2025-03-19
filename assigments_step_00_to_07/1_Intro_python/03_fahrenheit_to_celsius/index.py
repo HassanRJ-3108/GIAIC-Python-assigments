29ad9c22b6c017b3d3386074ae34e3bcc78c2cfd
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2025-03-19 13:21:40 +0500

    step1 part 3 completed fahrenheit to celsius converter

---
+++ assigments_step_00_to_07/1_Intro_python/03_fahrenheit_to_celsius/index.py
@@ -0,0 +1,14 @@
+def main():
+    # Main Heading
+    print("Welcom to fahrenheit to celsius converter")
+    # Get the temperature input in Fahrenheit
+    degrees_fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    # Convert the temperature to Celsius
+    degrees_celsius = (degrees_fahrenheit - 32) * 5.0/9.0
+    # Display the temperature in Celsius
+    print(f"Temperature: {degrees_fahrenheit}F = {degrees_celsius}C")
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
