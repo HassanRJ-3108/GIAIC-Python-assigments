9d28fd2b7d842651c553030a7ce96841b345a0c6
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2025-03-20 18:19:58 +0500

    feat: implement shorten function to limit list length and add user input functionality with problem statement

---
+++ assigments_step_00_to_07/03_lists/08_shorten/index.py
@@ -0,0 +1,23 @@
+def shorten(lst):
+    while len(lst) > 3:
+        lst.pop()
+    print(lst)
+
+def get_lst():
+    """
+    Prompts the user to enter one element of the list at a time and returns the resulting list.
+    """
+    lst = []
+    elem = input("Please enter an element of the list or press enter to stop. ")
+    while elem != "":
+        lst.append(elem)
+        elem = input("Please enter an element of the list or press enter to stop. ")
+    return lst
+
+def main():
+    lst = get_lst()
+    shorten(lst)
+
+
+if __name__ == '__main__':
+    main()
